2a63edc9ea5acb187ba0a8ff1c4416bbc83b6ac2
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2021-06-24 10:16:29 -0500

    using python

--- update_dns.sh
+++ update_dns.sh
@@ -1,8 +1,23 @@
-namespace=$(kubectl get ns | grep -i si3ms |  awk '{print $1}')
-loadBalancerURL=$(kubectl get svc --namespace=$namespace | grep LoadBalancer | awk '{print $4}')
+echo "install eksctl"  
+            curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | \
+            tar xz -C /tmp  
+            mv /tmp/eksctl /usr/local/bin                
+            
+            instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
-sed -i 's/"a.example.com"/service.si3mshady.com/g' CNAME.json
-sed -i 's/4.4.4.4/$loadBalancerURL/g' CNAME.json  
+            aws ec2 modify-instance-metadata-options \
+            --instance-id $instance_id \
+            --http-put-response-hop-limit 2 \
+            --http-endpoint enabled \
+            --region us-east-1 
 
-aws route53 change-resource-record-sets \
---hosted-zone-id Z099267523KVY5EITOQ5W --change-batch file://CNAME.json
+            python3 ./deployment_handler.py && namespace=$(kubectl get ns | grep -i si3ms |  awk '{print $1}') && \
+            
+            loadBalancerURL=$(kubectl get svc --namespace=$namespace | grep LoadBalancer | awk '{print $4}') && \
+            
+            sed -i 's/a.example.com/service.si3mshady.com/g' CNAME.json || true && echo 'pass'  && \
+            sed -i "s/8.8.8.8/$loadBalancerURL/g" CNAME.json  || true && echo 'pass'  && \
+
+            aws route53 change-resource-record-sets \
+            --hosted-zone-id Z099267523KVY5EITOQ5W \
+            --change-batch file://CNAME.json   
